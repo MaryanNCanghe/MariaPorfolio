8d9d7c88cabc3ada1b6483fd7f1bde18ac2543b9
Kiki <marianecanhe@gmail.com>
2025-04-14 21:05:15 +0200

    portfolio to porfolio

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/MariaPortfolio/',
+  base: '/MariaPorfolio/',
   resolve: {
     alias: {
       '@assets': '/src/assets', 
